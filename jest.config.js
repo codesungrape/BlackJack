249780d99f3ff7340f9a33c9c0cbda84f52704f9
codesungrape <miss.s.rai@outlook.com>
2025-02-14 20:37:27 +0000

    configured to show the test coverage chart on npm test.

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,36 @@
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     // Handle module aliases (if you're using them in your Next.js project)
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
-  }
-}
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
+  },
+  collectCoverageFrom: [
+    "src/components/**/*.{js, jsx, ts,tsx}",
+    "!src/components/**/*.test.{ts,tsx}",
+    "!src/components/ui/**",
+    "!src/components/**/theme-provider.tsx",
+  ],
+  testMatch: ["<rootDir>/src/**/*.test.{ts,tsx}"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  rootDir: ".",
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
