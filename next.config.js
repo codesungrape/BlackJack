49e420649a8f2eec1f375ed2abb013d10581393b
codesungrape <miss.s.rai@outlook.com>
2025-02-15 16:05:22 +0000

    removed all sentry related stuff as i want to re-add later.

--- next.config.js
+++ next.config.js
@@ -27,47 +27,4 @@ const nextConfig = {
   ...userConfig,
 };
 
-// Injected content via Sentry wizard below
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    org: "danielle-mcloughlin",
-    project: "hackathon",
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Automatically annotate React components to show their full name in breadcrumbs and session replay
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    automaticVercelMonitors: true,
-
-    // If you want to delete sourcemaps after upload, add:
-    sentryBuildOptions: {
-      deleteSourcemapsAfterUpload: true,
-    }
-  }
-);
+module.exports = nextConfig;
